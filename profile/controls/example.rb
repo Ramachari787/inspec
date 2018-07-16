eac295645238754c9a8985917427e66538bc1ea7
Ramachari787 <rampushpagiri@gmail.com>
2018-07-16 12:34:12 +0530

    Update example.rb

--- profile/controls/example.rb
+++ profile/controls/example.rb
@@ -20,13 +20,24 @@ control 'tmp-1.0' do                        # A unique ID for this control
   it { should exist}
 
   end
-  describe docker.containers.where { names == 'hello-world' } do
-  it { should be_running }
-  its('images') { should_not include 'u12:latest' }
-  it { should exist }
-end
+#  describe docker.containers.where { names == 'hello-world' } do
+ # it { should be_running }
+ # its('images') { should_not include 'u12:latest' }
+ # it { should exist }
+#end
 describe docker.version do
   its('Server.Version') { should cmp >= '18.03.1-ce'}
   its('Client.Version') { should eq  '1.12'}
 end
+
+describe powershell('curl -v https://google.com |findstr StatusCode') do
+   its('stdout') { should include '200' }
+end 
+
+#java -version
+
+ describe package('default-jdk') do
+    it { should be_installed }
+    its('version') { should match '1.8.0_171'  }
+  end
 end
